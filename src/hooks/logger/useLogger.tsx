55d46ac5527fce8bf14a2edb9da00979f36f1233
limeojin363 <2002eojin@gmail.com>
2024-01-26 20:13:06 +0900

    chore: 주석 추가

--- src/hooks/logger/useLogger.tsx
+++ src/hooks/logger/useLogger.tsx
@@ -1,11 +1,16 @@
 import { useCallback, useEffect, useState } from "react";
 
+/**
+ * 실행 로그를 보여주기 위한 hook
+ * log: console.log 대용
+ * show: 정답 보여주기 함수
+ * isLogShow: 정답을 보여 주나?
+ * result: view에 나타날 실행 결과
+ */
 const useLogger = <T extends number | string>() => {
   const [queue, setQueue] = useState<T[]>([]);
   const [result, setResult] = useState<T[]>([]);
   const [isLogShow, setIsFired] = useState(false);
-
-  const show = () => setIsFired(true);
 
   const pushToQueue = useCallback(
     (item: T) => {
@@ -28,7 +33,12 @@ const useLogger = <T extends number | string>() => {
     }
   }, [move, isLogShow]);
 
-  return { log: pushToQueue, isLogShow, show, result };
+  return {
+    log: pushToQueue, // 외부에는 log로 제공, 내부적으로는 큐에 밀어넣어 놓는 방식.
+    show: () => setIsFired(true),
+    isLogShow,
+    result,
+  };
 };
 
 export default useLogger;
