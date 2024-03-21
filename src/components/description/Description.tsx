480e2a4876b6b11c4796a641d96c1bc3cf4a685e
limeojin363 <2002eojin@gmail.com>
2024-03-22 00:09:20 +0900

    docs: update description

--- src/components/description/Description.tsx
+++ src/components/description/Description.tsx
@@ -2,9 +2,7 @@ const Description = () => {
   return (
     <div className="p-4 flex flex-col gap-1 text-gray-800">
       <h1 className="font-bold text-lg">Description</h1>
-      <ul>
-        <li>정답 입력 후 비교하는 기능도 제작 예정입니다.</li>
-      </ul>
+      ㅎㅇ요
     </div>
   );
 };
